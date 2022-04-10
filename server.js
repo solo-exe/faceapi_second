486deda6b4967a73d5a2123036a2a2dc414293f6
solomon-web <solomonabu90@gmail.com>
2022-04-11 00:47:59 +0100

    changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controller/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString : process.env.DATABASE_URL,
         // port: 5432,
         // user: 'postgres',
         // password: 'freetown234',
