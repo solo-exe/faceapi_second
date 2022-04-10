f470568e8cf0dc821a7397feb11a34818be783b1
solomon-web <solomonabu90@gmail.com>
2022-04-11 00:57:12 +0100

    changes

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const image = require('./controller/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString : process.env.DATABASE_URL,
-        // port: 5432,
-        // user: 'postgres',
-        // password: 'freetown234',
-        // database: 'smartbrain',
-        ssl: true
+        connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    // port: 5432,
+    // user: 'postgres',
+    // password: 'freetown234',
+    // database: 'smartbrain',
     }
 });
 
